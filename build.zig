d82336c48d59a20b7436f12818e557105825c267
David Newman <dnewman@twitter.com>
2019-06-03 22:12:53 -0700

    cross compile for linux maybe?

--- build.zig
+++ build.zig
@@ -3,11 +3,18 @@ const builtin = @import("builtin");
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
+    const linux = b.option(bool, "linux", "create linux build") orelse false;
 
     var exe = b.addExecutable("streamplot", "src/main.zig");
+
     exe.setBuildMode(mode);
     exe.setOutputDir("zig-cache");
+    exe.linkSystemLibrary("c");
     exe.linkSystemLibrary("ncurses");
+
+    if (linux) {
+        exe.setTarget(builtin.Arch.x86_64, builtin.Os.linux, builtin.Abi.gnu);
+    }
 
     b.default_step.dependOn(&exe.step);
     b.installArtifact(exe);
